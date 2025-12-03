bd4661bf25d1962fdc2bc8066e1c0f52f0ef1008
kacper-ziemecki <kacperziemecki@gmail.com>
2025-12-03 19:23:39 +0100

    szkoła

--- Codechef Problems/CHFIDEAL.cpp
+++ Codechef Problems/CHFIDEAL.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define endl "\n"
+// #define endl "\n"
+#define pb push_back
 #define ll long long
 #define ull unsigned long long
 #define ld long double
@@ -8,29 +9,29 @@ void dbg_out() { cout << endl; }
 template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
 #define dbg(...) cout << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
 
-
 void solve(){
-  ll n;
-  cout << 3;
-  cin >> n;
-  cout << 3;
-} 
- 
+  int x;
+  cout << 1 << endl;
+  cin >> x;
+  if(x == 2){
+    cout << 3 << endl;
+  } else if(x == 3){
+    cout << 2 << endl;
+  } 
+}
+
 int main()
 {
- 
-  ios_base::sync_with_stdio(0);
-  cin.tie(0);
-  cout.tie(0);
- 
-#ifndef ONLINE_JUDGE
-  freopen("../in.in", "r", stdin);
-  freopen("../out.out", "w", stdout);
-#endif
 
+//   ios_base::sync_with_stdio(0);
+//   cin.tie(0);
+//   cout.tie(0);
+
+// #ifndef ONLINE_JUDGE
+//   freopen("../../in.in", "r", stdin);
+//   freopen("../../out.out", "w", stdout);
+// #endif
 
   
   solve();
-  
-  
 }
