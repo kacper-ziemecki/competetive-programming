8bfeaf814f1fc6501cb7ba2699b1f7d50546a591
Kacper Ziemecki <kacperziemecki@gmail.com>
2025-12-03 19:26:04 +0100

    lekcje

--- lekcje/zer.cpp
+++ lekcje/zer.cpp
@@ -21,11 +21,46 @@ void solve(){
     if(lista[i] == 2){
       res[i] = 1;
       res[i+1] = 1;
-      
+      for(int j = i+2; j < n; j++){
+        res[j] = lista[j-1]-res[j-1];
+      }
+      for(int j = i-1; j >= 0; j--){
+        res[j] = lista[j]-res[j+1];
+      }
+      cout << n << endl;
+      for(auto el : res) cout << el << ' ';
+      cout << endl;
+      return;
     } else if(lista[i] == 0){
       res[i] = 0;
       res[i+1] = 0;
+      for(int j = i+2; j < n; j++){
+        res[j] = lista[j-1]-res[j-1];
+      }
+      for(int j = i-1; j >= 0; j--){
+        res[j] = lista[j]-res[j+1];
+      }
+      cout << n << endl;
+      for(auto el : res) cout << el << ' ';
+      cout << endl;
+      return;
     }
+  }
+  cout << "0 2" << endl;
+  cin >> suma;
+  cout << n << endl;
+  if(suma){
+    for(int i = 0; i < n; i++){
+      if(i&1) cout << "0 ";
+      else cout << "1 ";
+    }
+    cout << endl;
+  } else{
+    for(int i = 0; i < n; i++){
+      if(i&1) cout << "1 ";
+      else cout << "0 ";
+    }
+    cout << endl;
   }
 }
 
