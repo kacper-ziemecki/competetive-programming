c21fdb44575169a033771c151c5d8a505ab6fc77
kacper-ziemecki <kacperziemecki@gmail.com>
2026-02-28 12:59:54 +0100

    t-olimpians

--- wkowanie/main.cpp
+++ wkowanie/main.cpp
@@ -1,7 +1,33 @@
 #include<bits/stdc++.h>
+#include<ext/pb_ds/tree_policy.hpp>
+#include<ext/pb_ds/assoc_container.hpp>
 using namespace std;
-#define endl "\n"
+using namespace __gnu_pbds;
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+#define endl '\n'
 #define pb push_back
-void dbg_out(){ cout << endl; }
-template<typename Head, typename... Tail> void dbg_out(Head H,Tail... T){ cout << ' ' << H; dbg_out(T...);}
-#define dbg(...) cout << '(' << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+void dbg_out(){cout << endl;}
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T){cout <<' ' <<H;dbg_out(T...);}
+#define dbg(...) cout<<'('<<#__VA_ARGS__<<"):", dbg_out(__VA_ARGS__)
+
+int rnd(int a, int b){
+	return rand()%(b-a+1)+a;
+}
+
+void solve(){
+
+}
+
+int main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	freopen("./in.in", "r", stdin);
+
+	int seed;
+	cin >> seed;
+	srand(seed);
+
+	solve();
+}
--- wkowanie/test.sh
+++ wkowanie/test.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 for((i=1;;i++))
+do
 	echo $i > seed
 	./generator < seed > test.in 
+	./wzor < test.in > wzor.out  
 	./brut < test.in > brut.out 
-	./wzor < test.in > wzor.out 
 	if diff -b wzor.out brut.out > /dev/null
 	then
 		echo "OK" $i 
-	else
+	else 
 		echo "ZLE" $i 
 		break 
-	fi 
+	fi
 done
